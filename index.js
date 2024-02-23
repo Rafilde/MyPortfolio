a56b78a9b20c551ae8c04c40cbca9ba0d1c94a93
Rafael Silva <rafasilva182003@gmail.com>
2024-02-23 03:37:55 -0300

    adding security

--- index.js
+++ index.js
@@ -3,34 +3,24 @@ const fullName = document.getElementById('name');
 const userEmail = document.getElementById('email');
 const menssage = document.getElementById('menssage'); 
 
-async function getEmailConfig() {
-  const response = await fetch('config.json');
-  return await response.json();
-}
-
-async function sendEmail() {
-  const emailConfig = await getEmailConfig();
-
+function sendEmail() {
   const bodyMenssage = `Full Name: ${fullName.value} <br>
   User Email: ${userEmail.value} <br>
-  Menssage: ${menssage.value}`;
-  
-  Email.send({
-    Host : "smtp.elasticemail.com",
-    Username : emailConfig.email,
-    Password : emailConfig.senha,
-    To : emailConfig.email, 
-    From : emailConfig.email,
-    Subject : "Interested customer",
-    Body : bodyMenssage,
-  }).then(() => {
-    alert("Mensagem enviada com sucesso!");
-  });
-}
+  Menssage: ${menssage.value}`
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault(); 
-  await sendEmail(); 
+    Email.send({
+      SecureToken : "2885ea7f-50d0-4d8d-b413-5c57caf33985",
+      To : 'rafaneto090@gmail.com',
+        From : "rafaneto090@gmail.com",
+        Subject : "Interested customer",
+        Body : bodyMenssage,
+  }).then(
+    alert("Mensagem enviada com sucesso!"),
+  );
+}
+form.addEventListener("submit", (e) => {
+  e.preventDefault;
+  sendEmail();
   document.getElementById('name').value = "";
   document.getElementById('email').value = "";
   document.getElementById('menssage').value = "";
